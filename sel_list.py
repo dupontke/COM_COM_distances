3b5dae35658c4e57cc41d41d1e34dfda3ca16b26
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-08-30 18:58:30 -0600

    [atom selections for center of mass analysis]

---
+++ sel_list.py
@@ -0,0 +1,6 @@
+##!/mnt/lustre_fs/users/mjmcc/apps/python2.7/bin/python
+##!/Library/Frameworks/Python.framework/Versions/2.7/bin/python
+
+sel = []
+sel.append(['resid 50 and not backbone','resid 28 and not backbone','R50E28'])
+sel.append(['resid 50 and not backbone','resid 104 and not backbone','R50E104'])
