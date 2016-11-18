fa2d341f67f5240a706da098ef807f71d41d04f5
Kelly Du Pont <dupontke@enterprise.chm.colostate.edu>
2016-11-18 14:27:17 -0700

    [updated]

--- sel_list.py
+++ sel_list.py
@@ -4,3 +4,5 @@
 sel = []
 sel.append(['resid 50 and not backbone','resid 28 and not backbone','R50E28'])
 sel.append(['resid 50 and not backbone','resid 104 and not backbone','R50E104'])
+sel.append(['resid 18 and not backbone','resname GTP and name N9 C8 N7 C5 C6 O6 N1 C2 N2 N3 C4','R18gtp_base'])
+sel.append(['resid 50 and not backbone','resname GTP and name O1G PG O2G O3G','R50gtp_gamma_phosphate'])
